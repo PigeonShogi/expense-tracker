5c457eb0a9a21063580dabad4e4b7f6150b3913a
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-02 23:04:54 +0800

    modify: modify the routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', (req, res) => {
+  res.render('index')
+})
+
+router.get('/bootstrap', (req, res) => {
+  res.render('bootstrap')
+})
+
 module.exports = router
