ac41c85f5d8138accd2d5154e661668861cbbb26
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-03 21:27:59 +0800

    feat: add login page and register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,12 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
+router.get('/login', (req, res) => {
+  res.render('login')
+})
+
+router.get('/logout', (req, res) => {
+  res.redirect('/users/login')
+})
+
 module.exports = router
