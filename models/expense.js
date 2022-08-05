6a416b4560303c850f5c701d252d7af06665bab8
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-05 23:37:00 +0800

    feat: set the route of /expense/new

--- models/expense.js
+++ models/expense.js
@@ -4,13 +4,14 @@ const { Schema } = mongoose
 const expenseSchema = new Schema({
   name: { type: String, required: true },
   date: { type: Date, default: Date.now },
+  category: { type: String, required: true },
   amount: { type: Number, required: true },
   // 加入關聯設定
   userId: {
     type: Schema.Types.ObjectId,
     ref: 'User',
     index: true,
-    required: true
+    required: false
   },
   // categoryId: Number,
 })
--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -1,12 +1,18 @@
 const express = require('express') // 載入 Express
 const router = express.Router() // 載入 express.Router()
+const Expence = require('../../models/expense')
 
 router.get('/new', (req, res) => {
   res.render('new')
 })
 
 router.post('/new', (req, res) => {
-  res.render('new')
+  const { name, category, amount } = req.body
+  Expence.create({
+    name, date: Date(), category, amount
+  })
+    .then(() => res.redirect('/'))
+    .catch(err => console.error(err))
 })
 
 module.exports = router
