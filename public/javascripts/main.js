f7cd588a94668cd6c78025a60c5859f29bd1ddb5
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-08-29 09:48:27 +0800

    feat: adjust the background color of the displayed data

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+// 將首頁顯示的每一筆支出之底色調整為：奇數筆底色較深，偶數筆底色較淺。
+const divDark = document.querySelectorAll('.div-a-data-dark')
+divDark.forEach(element => {
+  if (Number(element.dataset.index) % 2 === 0) {
+    element.classList.remove('div-a-data-dark')
+    element.classList.add('div-a-data-light')
+  }
+})
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -12,12 +12,17 @@ router.get('/', (req, res) => {
     .sort({ _id: 'asc' })
     .then(expenses => {
       const expenses_id = []
+      let expenses_serialNumber = 1
       expenses.forEach(element => {
+        element.serialNumber = expenses_serialNumber
+        expenses_serialNumber++
+        // console.log(element)
         expenses_id.push(element._id)
         element.date = dateConvert(element.date)
       })
       res.locals.expenses_id = expenses_id
       const sum = amountSum(expenses)
+      // console.log('expenses ===', expenses)
       res.render('index', { expenses, sum })
     })
     .catch(err => console.error(err))
